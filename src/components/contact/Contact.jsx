2970399a7a7b942f685173246914f47b4f64aecd
Hein Htike Aung <niziu1151998@gmail.com>
2022-07-07 21:39:21 +0630

    commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './contact.scss'
+import React, { useState } from 'react';
+import './contact.scss';
 
 const Contact = () => {
-  return (
-    <div id='contact' className='contact'>Contact</div>
-  )
-}
+	const [message, setMessage] = useState(false);
 
-export default Contact
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setMessage(true);
+	};
+
+	return (
+		<div id='contact' className='contact'>
+			<div className='left'>
+				<img src='assets/shake.svg' alt='' />
+			</div>
+			<div className='right'>
+				<h2>Contact.</h2>
+				<form onSubmit={handleSubmit}>
+					<input type='text' placeholder='Email' />
+					<textarea placeholder='Message'></textarea>
+					<button type='submit'>Send</button>
+					{message ? (
+						<span>Thanks, I'll reply ASAP :)</span>
+					) : (
+						<span>&#8203;</span>
+					)}
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Contact;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from 'react';
 import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Intro = () => {
-  return (
-    <div id='intro' className='intro'>Intro</div>
-  )
-}
+	// const textRef = useRef();
 
-export default Intro
+	// useEffect(() => {
+	//   init(textRef.current, {
+	//     showCursor: true,
+	//     backDelay: 1500,
+	//     backSpeed:60,
+	//     strings: ["Developer", "Designer", "Content Creator"],
+	//   });
+	// }, []);
+
+	return (
+		<div id='intro' className='intro'>
+			<div className='left'>
+				<div className='imgContainer'>
+					<img src='assets/xiaoting.png' alt='' />
+				</div>
+			</div>
+			<div className='right'>
+				<div className='wrapper'>
+					<h2>Hi There, I'm</h2>
+					<h1>Xiaoting</h1>
+					{/* <h3>
+            Freelance <span ref={textRef}></span>
+          </h3> */}
+					<div className='titleContainer'>
+						<h3>Freelance</h3>
+						<h4>
+							<Typewriter
+								options={{
+									strings: ['Developer', 'Designer', 'Content Creator'],
+									autoStart: true,
+									delay: 75,
+									loop: true,
+								}}
+							/>
+						</h4>
+					</div>
+				</div>
+				<a href='#portfolio'>
+					<img src='assets/down.png' alt='' />
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Intro;
